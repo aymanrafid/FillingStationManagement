d89190976f29750d0fb2fb43fcc6d74931519d35
aymanrafid <kazi.sachcha@northsouth.edu>
2022-12-31 20:05:21 +0600

    add splash.h and image

--- MetroFillingStation/main.c
+++ MetroFillingStation/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dos.h>
 #include "login.h"
 #include "price.h"
 #include "invoice.h"
 #include "menu.h"
-int main(){
+#include"splash.h"
+
+
+int main(void){
+      splash();
+      sleep(5);
+      system("cls");
       show_menu();
       return 0;
 }
