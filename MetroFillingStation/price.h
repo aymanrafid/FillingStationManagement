43f27f505d87d8f7e20caa82895fe1bb06f257df
aymanrafid <kazi.sachcha@northsouth.edu>
2022-12-31 16:05:27 +0600

    addded price.h

---
+++ MetroFillingStation/price.h
@@ -0,0 +1,50 @@
+double price(int n) {
+    FILE *pointer;
+
+    pointer=fopen("Prices.txt","r");
+    if(pointer == NULL){
+        printf("Prices file couldn't be opened\n");
+        return -1;
+    }
+    else{
+        double p, c, d, o;
+        fscanf(pointer,"%lf %lf %lf %lf",&p,&c,&d,&o);
+        fflush(stdin);
+        fclose(pointer);
+
+        if(n==1) return p;
+        else if(n==2) return c;
+        else if(n==3) return d;
+        else if(n==4) return o;
+    }
+    return -1;
+}
+
+int update_prices() {
+     system("cls");
+     printf("      Update new Prices\n\n\n");
+
+     double p, c, d, o;
+     printf("PETROL: ");
+     scanf("%lf",&p);
+     printf("CNG: ");
+     scanf("%lf",&c);
+     printf("DIESEL: ");
+     scanf("%lf",&d);
+     printf("OCTANE: ");
+     scanf("%lf",&o);
+
+
+     FILE *pointer;
+     pointer=fopen("Prices.txt","w");
+     if(pointer == NULL){
+        printf("Prices file couldn't be opened\n");
+        return 0;
+     }
+     else{
+        fprintf(pointer,"%lf %lf %lf %lf",p,c,d,o);
+     }
+
+     fclose(pointer);
+     return 1;
+}
